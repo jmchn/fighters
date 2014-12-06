83521288575fa72ddd494d988c17694bd1270cb7
jianming <jmchn@umich.edu>
2014-12-06 18:34:50 -0500

    s

--- search.php
+++ search.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html>
+.a{
+    display:none;
+}
 <body>
 	<form name="input" action="search.php" method="get">
 	Search: <input type="text" name="query">
@@ -14,22 +17,18 @@
 		$myResults = queryIndex($portNumber, "localhost", $_GET['query'],$_GET['slider1']);
 		//echo $myResults[0]['id'];		
 		//var_dump($myResults);
-		echo "We have found ".count($myResults)."results";
-		echo "<table border='1' style='width:100%'>";
-		echo "<tr>";
-		echo "<td>sequenceNumber</td>";
-		echo "<td>Caption</td>";
-		echo "<td>Thumbnail</td>";
-		echo "</tr>";
 		$con = mysqli_connect("localhost","group51","test","group51");
 		for( $i = 0 ; $i<count($myResults) && $i<10 ;$i++)
         {
-            echo $myResults[$i];
+
+            echo "The Url is:<a href='http://en.wikipedia.org/wiki?curid=".$myResults[$i]['id']."'>http://en.wikipedia.org/wiki?curid=".$myResults[$i]['id']."</a>";
+            echo "<div class='a'>";
+            echo "</div>";
 			//do it in naive way
 			//each time Got a docid 
 			// query the database to retrieve the info we need
 			// the sequence
-			//$query = mysqli_query($con,"select * from PhotoPA4 where sequencenum =".$myResults[$i]['id']);
+			$query = mysqli_query($con,"select * from imageUrl where id ='".$myResults[$i]['id']."'");
 			//$result = mysqli_fetch_array($query);
 			//echo "<tr>";
 			//echo "<td>".$result["sequencenum"]."</td>";
@@ -46,4 +45,9 @@
 ?>
 
 </body>
+<script>
+    function hideTR(){
+        //change the CSS style property to 'none' for hiding, or 'inline' to show    
+        document.getElementById('hideThis').style.display='none';}
+</script>
 </html>
